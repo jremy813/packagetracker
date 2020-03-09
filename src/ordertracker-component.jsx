3bff30744a87b17fd41c3283ca1d0c213b3585ff
jremy813 <jremywatkins@gmail.com>
2020-03-08 23:19:57 -0400

    added html structure

---
+++ src/ordertracker-component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <div id="header1"></div>
+      <div id="ordertracker"></div>
+      <div id="trackingdetails"></div>
+    </div>
+  );
+}
+
+export default App;
