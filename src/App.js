dd6ae07a6625e58130910d3622c817079381282a
jremy813 <jremywatkins@gmail.com>
2020-03-15 09:04:37 -0400

    added second prop nd deleted useless files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Tracker from "./ordertracker-component";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Tracker date="Monday, the 5th" status="out for delivery" />
+    </div>
+  );
 }
 
 export default App;
--- src/ordertracker-component.jsx
+++ src/ordertracker-component.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import "./ordertracker-component.css";
 
-function App() {
+function Tracker(props) {
   return (
-    <div className="App">
-      <div id="header1"></div>
-      <div id="ordertracker"></div>
+    <div className="Tracker">
+      <div id="header1">
+        <div>
+          <h2>Out for deliv</h2>
+          <span>Expected Delivery: {props.date}</span>
+        </div>
+        <div>{props.status}</div>
+      </div>
+      <div id="ordertracker">
+        <ol class="progtrckr" data-progtrckr-steps="5">
+          <li class="progtrckr-done">Order Processing</li>
+          <li class="progtrckr-done">Pre-Production</li>
+          <li class="progtrckr-done">In Production</li>
+          <li class="progtrckr-todo">Shipped</li>
+          <li class="progtrckr-todo">Delivered</li>
+        </ol>
+      </div>
       <div id="trackingdetails"></div>
     </div>
   );
 }
 
-export default App;
+export default Tracker;
